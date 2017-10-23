aef20180c88debef584353b9fe4a0a8177faaf0a
Roman Stankiewicz <roman.stan3@gmail.com>
2017-10-23 12:02:19 +0100

    mongo remote env

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,9 @@
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'development';
-const dbURI = process.env.MONGODB_URI || `mongodb://localhost/hw-w07d01-${env}`;
+// const dbURI = process.env.MONGODB_URI || `mongodb://localhost/hw-w07d01-${env}`;
+const admin = process.env.SPECSAVER_ADMIN_USERNAME
+const password = process.env.SPECSAVER_ADMIN_PASSWORD
+const dbURI = process.env.MONGODB_URI || `mongodb://${admin}:${password}@ds129315.mlab.com:29315/specsaver-appointments`;
+
 
 module.exports = { port, env, dbURI };
