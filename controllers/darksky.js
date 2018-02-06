a089aad70eeb2aaf5c1e04530ef37a2a31028d21
Roman Stankiewicz <roman.stan3@gmail.com>
2018-02-06 11:25:31 +0000

    dark sky route bug fixes

--- controllers/darksky.js
+++ controllers/darksky.js
@@ -3,8 +3,12 @@ var fetch = require('node-fetch');
 function fetchWeather(req, res) {
     const latlng = req.params.latlng
     return fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/${latlng}`)
-    .then((resp) => resp.json())
-    .catch(error => console.log(error))
+    .then(resp => resp.json())
+    .then(data => res.json(data.currently.summary))
+    .catch(error => {
+        console.log(error)
+        return res.json(error)
+    })
 }
 
 module.exports = {
